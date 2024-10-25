5e93788f6802583fd120d992e4df53c0554b909a
IMMANUEL_DOMINIC OGABOR_ODAMA <immanuel.dynamic@alumnes.ilerna.com>
2024-10-25 16:14:53 +0200

    corregido errores en pom.xml y hecha la conexion con contenedor docker local en vez de bbdd online

--- QuizMania/src/main/java/abp/project/Constants.java
+++ QuizMania/src/main/java/abp/project/Constants.java
@@ -3,8 +3,8 @@ package abp.project;
 public class Constants {
 
 
-    public static final String USER = "u8k30tjjl3kpxbzf";
-    public static final String PASS = "9wzAOZH3Vju4przL6IkX";
+    public static final String USER = "root";
+    public static final String PASS = "root";
     public static final String SCHEMA = "conexion";
-    public static  final String BBDD = "jdbc:mysql://bme0gdqjzi97fimbflag-mysql.services.clever-cloud.com:3306/bme0gdqjzi97fimbflag?";
+    public static  final String BBDD = "jdbc:mysql://127.0.0.1:3306/conexion";
 }
