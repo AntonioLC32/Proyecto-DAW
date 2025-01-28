3523ed46bcddb6e2916a6546c3b4e18e1de6163d
antoniolc <antoniolucenilla32.com>
2025-01-28 15:54:45 +0100

    estructura vue

---
+++ quizmania/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+      </ul>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ quizmania/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Bienvenido a la página de inicio</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
